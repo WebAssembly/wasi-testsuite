839012dedfeacb0e66c6f478cd8d05c1c367648c
Callum Macmillan🍺 <cmmacmil@amazon.com>
2022-11-10 10:47:31 +0000

    Using public NPM registry

--- tests/core/buildscripts/utility.ts
+++ tests/core/buildscripts/utility.ts
@@ -2,7 +2,7 @@ import * as childProcess from "child_process";
 import * as fs from "fs";
 import * as path from "path";
 
-export async function execAsPromise(command: string) {
+export async function execAsPromise(command: string): Promise<void> {
     return new Promise<void>((resolve, reject) => {
         childProcess.exec(command, (error) => {
             error ? reject(error) : resolve();
@@ -10,14 +10,14 @@ export async function execAsPromise(command: string) {
     });
 }
 
-export function getPathsInDirectory(directory: string) {
+export function getPathsInDirectory(directory: string): string[] {
     return fs.readdirSync(directory).map(filepath => path.join(directory, filepath));
 }
 
-export function doesFileHaveExtension(filepath: string, extension: string) {
-    return filepath.match(`^.*\.${extension}$`);
+export function doesFileHaveExtension(filepath: string, extension: string): boolean {
+    return !!filepath.match(`^.*\\.${extension}$`);
 }
 
-export function mapFileExtension(filepath: string, oldExtension: string, newExtension: string) {
+export function mapFileExtension(filepath: string, oldExtension: string, newExtension: string): string {
     return path.join(path.dirname(filepath), path.basename(filepath, oldExtension) + newExtension);
 }
