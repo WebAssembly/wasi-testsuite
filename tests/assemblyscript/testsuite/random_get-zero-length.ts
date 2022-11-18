cabb3ac530b072b9e7620214d4b97de36b74a840
Marcin Kolny <mkolny@amazon.com>
2022-11-18 16:50:21 +0000

    Add few more assemblyscript tests (environ_*, random_get)

---
+++ tests/assemblyscript/testsuite/random_get-zero-length.ts
@@ -0,0 +1,11 @@
+import {
+    random_get,
+    errno
+} from '@assemblyscript/wasi-shim/assembly/bindings/wasi_snapshot_preview1';
+
+const bufSize = 0;
+const buf = __alloc(bufSize);
+
+assert(errno.SUCCESS == random_get(buf, bufSize));
+
+__free(buf);
