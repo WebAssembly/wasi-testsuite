4500f83e601fed37cde6589a683354c8bc0df85c
Marcin Kolny <mkolny@amazon.com>
2022-11-18 16:48:24 +0000

    Compile AssemblyScript file only if it's outdated (i.e. WASM file doesn't exist or is older than the last modification of the source file)

--- tests/assemblyscript/buildscripts/build.ts
+++ tests/assemblyscript/buildscripts/build.ts
@@ -1,15 +1,29 @@
-import * as process from "process"
-import { doesFileHaveExtension, execAsPromise, getPathsInDirectory, mapFileExtension } from "./utility";
+import * as process from "process";
+import { doesFileHaveExtension, execAsPromise, getFileModifiedTimestamp, getPathsInDirectory, mapFileExtension } from "./utility";
 
 async function compileWithAsc(inputFilePath: string, outputFilePath: string) {
+    console.log(`Compiling ${inputFilePath}`);
     return execAsPromise(`npm run asc --silent -- "${inputFilePath}" -o "${outputFilePath}"`);
+}
+
+function isBinaryOutdated(srcPath: string, wasmPath: string): boolean {
+    const sourceTs = getFileModifiedTimestamp(srcPath);
+    if (!sourceTs) {
+        throw new Error(`Source file ${srcPath} doesn't exist`);
+    }
+    const wasmTs = getFileModifiedTimestamp(wasmPath);
+
+    return !wasmTs || sourceTs > wasmTs;
 }
 
 async function compileTests() {
     const testSuiteFiles = getPathsInDirectory("testsuite");
-    const testFiles = testSuiteFiles.filter(filepath => doesFileHaveExtension(filepath, "ts"));
-    const pendingCompilations = testFiles.map(filepath => compileWithAsc(filepath,  mapFileExtension(filepath, "ts", "wasm")));
-    return Promise.all(pendingCompilations);
+    const pendingCompilations = testSuiteFiles
+        .filter(filePath => doesFileHaveExtension(filePath, "ts"))
+        .map(filePath => ({ src: filePath, wasm: mapFileExtension(filePath, "ts", "wasm")}))
+        .filter(files => isBinaryOutdated(files.src, files.wasm))
+        .map(files => compileWithAsc(files.src, files.wasm));
+    return Promise.allSettled(pendingCompilations);
 }
 
 compileTests()
--- tests/assemblyscript/buildscripts/utility.ts
+++ tests/assemblyscript/buildscripts/utility.ts
@@ -10,14 +10,21 @@ export async function execAsPromise(command: string): Promise<void> {
     });
 }
 
+export function getFileModifiedTimestamp(filePath: string): number | undefined {
+    if (!fs.existsSync(filePath)) {
+        return undefined;
+    }
+    return fs.statSync(filePath).mtime.getTime();
+}
+
 export function getPathsInDirectory(directory: string): string[] {
     return fs.readdirSync(directory).map(filepath => path.join(directory, filepath));
 }
 
 export function doesFileHaveExtension(filepath: string, extension: string): boolean {
-    return !!filepath.match(`^.*\\.${extension}$`);
+    return filepath.endsWith(`.${extension}`);
 }
 
 export function mapFileExtension(filepath: string, oldExtension: string, newExtension: string): string {
-    return path.join(path.dirname(filepath), path.basename(filepath, oldExtension) + newExtension);
+    return filepath.replace(new RegExp(`(^.*)\\.${oldExtension}$`), `$1.${newExtension}`);
 }
