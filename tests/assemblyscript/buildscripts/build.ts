e78991c33e65e3d2b1c82b1db28d7461338265e8
Marcin Kolny <mkolny@amazon.com>
2022-11-22 09:13:59 +0000

    Fix build script for assemblyscript so it correctly reports compilation failures

--- tests/assemblyscript/buildscripts/build.ts
+++ tests/assemblyscript/buildscripts/build.ts
@@ -20,15 +20,32 @@ async function compileTests() {
     const testSuiteFiles = getPathsInDirectory("testsuite");
     const pendingCompilations = testSuiteFiles
         .filter(filePath => doesFileHaveExtension(filePath, "ts"))
-        .map(filePath => ({ src: filePath, wasm: mapFileExtension(filePath, "ts", "wasm")}))
+        .map(filePath => ({ src: filePath, wasm: mapFileExtension(filePath, "ts", "wasm") }))
         .filter(files => isBinaryOutdated(files.src, files.wasm))
-        .map(files => compileWithAsc(files.src, files.wasm));
-    return Promise.allSettled(pendingCompilations);
+        .map(files => compileWithAsc(files.src, files.wasm)
+            .then(_ => undefined)
+            .catch(error => ({ source: files.src, error })));
+
+    const errors = (await Promise.allSettled(pendingCompilations))
+        .map(p => {
+            if (p.status === 'fulfilled' && p.value !== undefined) {
+                return `Failed to compile ${p.value.source}:\n${p.value.error}`;
+            } else if (p.status === 'rejected') {
+                return `Execution failed:\n${p.reason}`;
+            } else {
+                return undefined;
+            }
+        }).filter(p => p !== undefined) as string[];
+
+    if (errors.length > 0) {
+        throw new Error(errors.join('\n\n'));
+    }
 }
 
 compileTests()
     .then(() => console.log("Tests compiled"))
     .catch((e) => {
-        console.error(`Tests failed to compile: ${e}`);
+        console.error(`Tests failed to compile:`);
+        console.error(e);
         process.exit(1);
     });
