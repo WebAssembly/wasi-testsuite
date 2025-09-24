6ade304cd66b39ddfd00d4b80a6a02a6ae86e7e1
Andy Wingo <wingo@igalia.com>
2025-09-24 10:17:04 +0200

    Add adapter for pywasm
    Also add run-pywasm executable script, as pywasm doesn't have one.
    Update workflow appropriately.

---
+++ adapters/pywasm.py
@@ -0,0 +1,35 @@
+import subprocess
+import os
+import shlex
+from pathlib import Path
+from typing import Dict, List, Tuple
+
+# shlex.split() splits according to shell quoting rules
+RUN_PYWASM = Path(__file__).parent.parent / "tools" / "run-pywasm"
+
+
+def get_name() -> str:
+    return "pywasm"
+
+
+def get_version() -> str:
+    # ensure no args when version is queried
+    result = subprocess.run([RUN_PYWASM, "--version"],
+                            encoding="UTF-8", capture_output=True,
+                            check=True)
+    output = result.stdout.splitlines()[0].split(" ")
+    return output[1]
+
+
+def compute_argv(test_path: str,
+                 args: List[str],
+                 env: Dict[str, str],
+                 dirs: List[Tuple[Path, str]]) -> List[str]:
+    argv = [str(RUN_PYWASM)]
+    for k, v in env.items():
+        argv += ["--env", f"{k}={v}"]
+    for host, guest in dirs:
+        argv += ["--dir", f"{host}::{guest}"]  # noqa: E231
+    argv += [test_path]
+    argv += args
+    return argv
