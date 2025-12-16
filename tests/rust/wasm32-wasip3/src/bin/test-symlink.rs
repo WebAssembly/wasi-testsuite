0101faedf0d98e7a126c8eeebc6a76a9ee563f52
Andy Wingo <wingo@igalia.com>
2025-12-16 14:39:28 +0100

    Add isolated symlink test

---
+++ tests/rust/wasm32-wasip3/src/bin/test-symlink.rs
@@ -0,0 +1,44 @@
+use std::process;
+extern crate wit_bindgen;
+
+wit_bindgen::generate!({
+    inline: r"
+  package test:test;
+
+  world test {
+      include wasi:filesystem/imports@0.3.0-rc-2025-09-16;
+      include wasi:cli/command@0.3.0-rc-2025-09-16;
+  }
+",
+    additional_derives: [PartialEq, Eq, Hash, Clone],
+    // Work around https://github.com/bytecodealliance/wasm-tools/issues/2285.
+    features:["clocks-timezone"],
+    generate_all
+});
+
+async fn test_symlink() {
+    match &wasi::filesystem::preopens::get_directories()[..] {
+        [(dir, _)] => {
+            dir.symlink_at("a.txt".to_string(), "b".to_string())
+                .await
+                .unwrap();
+        }
+        [..] => {
+            eprintln!("usage: run with one open dir");
+            process::exit(1)
+        }
+    }
+}
+
+struct Component;
+export!(Component);
+impl exports::wasi::cli::run::Guest for Component {
+    async fn run() -> Result<(), ()> {
+        test_symlink().await;
+        Ok(())
+    }
+}
+
+fn main() {
+    unreachable!("main is a stub");
+}
