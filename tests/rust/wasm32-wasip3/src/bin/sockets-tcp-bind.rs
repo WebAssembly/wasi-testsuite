0b260391048d260094a2328a223ea56f9c1a3a1d
Saúl Cabrera <saulecabrera@gmail.com>
2026-01-16 11:31:31 -0500

    sockets: Add test for ephemeral random port assignment

--- tests/rust/wasm32-wasip3/src/bin/sockets-tcp-bind.rs
+++ tests/rust/wasm32-wasip3/src/bin/sockets-tcp-bind.rs
@@ -8,6 +8,7 @@ wit_bindgen::generate!({
 	}
     ",
     features:["clocks-timezone"],
+    additional_derives: [PartialEq, Eq],
     generate_all
 });
 
@@ -38,6 +39,20 @@ impl IpSocketAddress {
             }),
         }
     }
+
+    fn ip_addr(&self) -> IpAddress {
+        match self {
+            IpSocketAddress::Ipv6(addr) => IpAddress::Ipv6(addr.address),
+            IpSocketAddress::Ipv4(addr) => IpAddress::Ipv4(addr.address),
+        }
+    }
+
+    fn port(&self) -> u16 {
+        match self {
+            IpSocketAddress::Ipv6(addr) => addr.port,
+            IpSocketAddress::Ipv4(addr) => addr.port,
+        }
+    }
 }
 
 fn test_invalid_address_family(family: IpAddressFamily) {
@@ -52,10 +67,28 @@ fn test_invalid_address_family(family: IpAddressFamily) {
     assert!(matches!(result, Err(ErrorCode::InvalidArgument)));
 }
 
+fn test_ephemeral_port_assignment(family: IpAddressFamily) {
+    let sock = TcpSocket::create(family).unwrap();
+
+    let addr = match family {
+        IpAddressFamily::Ipv4 => IpSocketAddress::new(IpAddress::Ipv4(IPV4_LOCALHOST), 0),
+        IpAddressFamily::Ipv6 => IpSocketAddress::new(IpAddress::Ipv6(IPV6_LOCALHOST), 0),
+    };
+
+    sock.bind(addr).unwrap();
+    let bound = sock.get_local_address().unwrap();
+
+    assert_eq!(addr.ip_addr(), bound.ip_addr());
+    // Randomly assigned port.
+    assert_ne!(addr.port(), bound.port());
+}
+
 impl exports::wasi::cli::run::Guest for Component {
     async fn run() -> Result<(), ()> {
         test_invalid_address_family(IpAddressFamily::Ipv4);
         test_invalid_address_family(IpAddressFamily::Ipv6);
+        test_ephemeral_port_assignment(IpAddressFamily::Ipv4);
+        test_ephemeral_port_assignment(IpAddressFamily::Ipv6);
         Ok(())
     }
 }
