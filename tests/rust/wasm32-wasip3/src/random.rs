fb19e1930fad7ffcd8c361760122ab6b24043784
Saúl Cabrera <saulecabrera@gmail.com>
2026-01-28 14:41:00 -0500

    Add tests for the `random` proposal

---
+++ tests/rust/wasm32-wasip3/src/random.rs
@@ -0,0 +1,15 @@
+wit_bindgen::generate!({
+    inline: r"
+	package wasi-testsuite:test;
+
+	world test {
+	    include wasi:random/imports@0.3.0-rc-2026-01-06;
+		include wasi:cli/command@0.3.0-rc-2026-01-06;
+	}
+    ",
+    features:["clocks-timezone"],
+    pub_export_macro: true,
+    default_bindings_module: "test_wasm32_wasip3::random",
+    additional_derives: [PartialEq, Eq],
+    generate_all
+});
