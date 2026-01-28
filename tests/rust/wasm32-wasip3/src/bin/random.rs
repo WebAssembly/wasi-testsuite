4656b9b73430a18d95f39b59007fd7ec55e97c6d
Saúl Cabrera <saulecabrera@gmail.com>
2026-01-28 14:41:00 -0500

    Add tests for the `random` proposal

---
+++ tests/rust/wasm32-wasip3/src/bin/random.rs
@@ -0,0 +1,53 @@
+use test_wasm32_wasip3::random::{
+    export,
+    exports::wasi::cli::run::Guest,
+    wasi::random::{insecure, insecure_seed, random},
+};
+
+struct Component;
+
+export!(Component);
+
+fn test_get_random_bytes() {
+    assert_eq!(random::get_random_bytes(100).len(), 100);
+    let a = random::get_random_bytes(10);
+    let b = random::get_random_bytes(10);
+    // Should always return fresh data.
+    assert_ne!(a, b);
+}
+
+fn test_get_random_u64() {
+    let a = random::get_random_u64();
+    let b = random::get_random_u64();
+    // Should always return fresh data.
+    assert_ne!(a, b);
+}
+
+fn test_get_insecure_seed() {
+    let (a, b) = insecure_seed::get_insecure_seed();
+    let (a1, b1) = insecure_seed::get_insecure_seed();
+
+    // Meant to be only called once.
+    // Any subsequent calls should return the same value.
+    assert_eq!(a, a1);
+    assert_eq!(b, b1);
+}
+
+fn test_get_insecure_random_bytes() {
+    let a = insecure::get_insecure_random_bytes(100);
+    assert_eq!(a.len(), 100);
+}
+
+impl Guest for Component {
+    async fn run() -> Result<(), ()> {
+        test_get_random_bytes();
+        test_get_random_u64();
+        test_get_insecure_seed();
+        test_get_insecure_random_bytes();
+        Ok(())
+    }
+}
+
+fn main() {
+    unreachable!();
+}
--- tests/rust/wasm32-wasip3/src/lib.rs
+++ tests/rust/wasm32-wasip3/src/lib.rs
@@ -1,3 +1,4 @@
 pub mod cli;
 pub mod clocks;
+pub mod random;
 pub mod sockets;
