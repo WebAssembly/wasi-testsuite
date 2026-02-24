625f567fb5b11b4482f22459f87eaa834576a8b0
Andy Wingo <wingo@igalia.com>
2026-02-24 16:49:02 +0100

    Refactor test runner
    When adding HTTP support to the runner, I got a bit distracted about
    code duplication, and how we had so much state in the run_test method,
    then realized that all the op interpretation isn't the responsibility of
    the runtime adapter, rather of the test case... and ended up with this
    big honkin' patch.

--- test-runner/wasi_test_runner/test_case.py
+++ test-runner/wasi_test_runner/test_case.py
@@ -1,6 +1,5 @@
 import logging
 import json
-import socket
 from pathlib import Path
 from enum import StrEnum
 from typing import List, NamedTuple, TypeVar, Type, Dict, Any, Set, Tuple
@@ -181,8 +180,6 @@ T = TypeVar("T", bound="Config")
 class Config(NamedTuple):
     # List of operations.
     operations: List[Operation] = [Run(), Wait()]
-    # Mapping of declared connections
-    connections: Dict[str, socket.socket] = {}
     # WASI proposals needed for the test.
     proposals: List[WasiProposal] = []
 
@@ -203,11 +200,7 @@ class Config(NamedTuple):
             if dict_config.get("proposals") is not None:
                 proposals = cls._proposals_from_config(dict_config.get("proposals"))
 
-            return cls(
-                operations=operations,
-                connections={},
-                proposals=proposals
-            )
+            return cls(operations=operations, proposals=proposals)
 
         cls._validate_config(dict_config, LEGACY_CONFIG_KEYS)
 
@@ -234,7 +227,6 @@ class Config(NamedTuple):
 
         return cls(
             operations=legacy_operations,
-            connections={},
             # Tests which require explicit proposals must be
             # configured using the new configuration.
             # See http-response.json
@@ -254,48 +246,6 @@ class Config(NamedTuple):
 
     def proposals_as_str(self) -> List[str]:
         return [p.value for p in self.proposals]
-
-    # Performs a dry run of the configuration validating its structure.
-    def dry_run(self) -> None:
-        run_found = False
-        procs: List[str] = []
-        errors: List[str] = []
-
-        for op in self.operations:
-            match op:
-                case Run() as run:
-                    if run_found:
-                        errors.append(f"{run}: each Run operation must be paired with a Wait operation")
-                    run_found = True
-                case Read() as read:
-                    if not run_found:
-                        errors.append(f"{read}: Found Read operation before Run")
-                case Write() as write:
-                    if not run_found:
-                        errors.append(f"{write}: Found Write operation before Run")
-                case Wait() as wait:
-                    if not run_found:
-                        errors.append(f"{wait}: Found Wait operation before Run")
-                    run_found = False
-                case Connect(conn_id, protocol_type) as conn:
-                    if not run_found:
-                        errors.append(f"{conn}: Found Connect operation before Run")
-                    if protocol_type != ProtocolType.TCP:
-                        errors.append(f"{conn}: {protocol_type} not supported")
-                    if conn_id in procs:
-                        errors.append(f"{conn}: Duplicate definition of id {conn_id}")
-                    procs.append(conn_id)
-                case Send(send_id) as send:
-                    if not run_found:
-                        errors.append(f"{send}: Found Send operation before Run")
-                    if send_id not in procs:
-                        errors.append(f"{send}: No identifier defined for {send_id}")
-                case Recv() as recv:
-                    if not run_found:
-                        errors.append(f"{recv}: Found Recv operation before Run")
-
-        if errors:
-            raise ValueError("\n".join(errors))
 
     @classmethod
     def _validate_config(cls: Type[T], dict_config: Dict[str, Any], expected_keys: Set[str]) -> None:
@@ -347,3 +297,154 @@ class TestCase(NamedTuple):
     config: Config
     result: Result
     duration_s: float
+
+
+class TestCaseRunnerBase:
+    config: Config
+    _failures: List[Failure]
+
+    def __init__(self, config: Config) -> None:
+        self.config = config
+        self._failures = []
+
+    def do_run(self, run: Run) -> None:
+        raise NotImplementedError()
+
+    def do_write(self, write: Write) -> None:
+        raise NotImplementedError()
+
+    def do_read(self, read: Read) -> None:
+        raise NotImplementedError()
+
+    def do_wait(self, wait: Wait) -> None:
+        raise NotImplementedError()
+
+    def do_connect(self, conn: Connect) -> None:
+        raise NotImplementedError()
+
+    def do_send(self, send: Send) -> None:
+        raise NotImplementedError()
+
+    def do_recv(self, recv: Recv) -> None:
+        raise NotImplementedError()
+
+    def do_cleanup(self, successful: bool) -> None:
+        raise NotImplementedError()
+
+    def as_result(self) -> Result:
+        failures, self._failures = self._failures, []
+        return Result(is_executed=True, failures=failures)
+
+    def has_failure(self) -> bool:
+        return bool(self._failures)
+
+    def run(self) -> Result:
+        successful = False
+        try:
+            for op in self.config.operations:
+                if self.has_failure():
+                    break
+
+                # The isinstance asserts in these clauses might seem
+                # redudant, given the match.  Asserts merely exist to
+                # ensure that mypy can fully resolve the underlying
+                # type; else it will report errors like:
+                #   wasi_test_runner/runtime_adapter.py:131: error: Argument 2 to "_handle_read"
+                #   has incompatible type "Read"; expected "Read" [arg-type]
+                match op:
+                    case Run() as run:
+                        assert isinstance(run, Run)
+                        self.do_run(run)
+                    case Write() as write:
+                        assert isinstance(write, Write)
+                        self.do_write(write)
+                    case Read() as read:
+                        assert isinstance(read, Read)
+                        self.do_read(read)
+                    case Wait() as wait:
+                        assert isinstance(wait, Wait)
+                        self.do_wait(wait)
+                    case Connect() as conn:
+                        assert isinstance(conn, Connect)
+                        self.do_connect(conn)
+                    case Send() as send:
+                        assert isinstance(send, Send)
+                        self.do_send(send)
+                    case Recv() as recv:
+                        assert isinstance(recv, Recv)
+                        self.do_recv(recv)
+
+            successful = not self.has_failure()
+        finally:
+            self.do_cleanup(successful)
+
+        return self.as_result()
+
+
+class TestCaseValidator(TestCaseRunnerBase):
+    _config_path: str
+    _has_proc: bool
+    _streams: Set[str]
+
+    def __init__(self, config: Config, config_path: str) -> None:
+        TestCaseRunnerBase.__init__(self, config)
+        self._config_path = config_path
+        self._has_proc = False
+        self._streams = set()
+
+    def assert_proc(self, op: Any) -> None:
+        assert self._has_proc, f"{op}: no process running"
+
+    def assert_no_proc(self, op: Any) -> None:
+        assert not self._has_proc, f"{op}: process still running"
+
+    def assert_stream(self, op: Any, name: str) -> None:
+        assert name in self._streams, f"{op}: no such stream: {name}"
+
+    def assert_no_stream(self, op: Any, name: str) -> None:
+        assert name not in self._streams, f"{op}: stream exists: {name}"
+
+    def do_run(self, run: Run) -> None:
+        self.assert_no_proc(run)
+        self._has_proc = True
+
+    def do_write(self, write: Write) -> None:
+        self.assert_proc(write)
+        match write.id:
+            case "stdin": pass
+            case name: self.assert_stream(write, name)
+
+    def do_read(self, read: Read) -> None:
+        self.assert_proc(read)
+        match read.id:
+            case "stdout": pass
+            case "stderr": pass
+            case name: self.assert_stream(read, name)
+
+    def do_wait(self, wait: Wait) -> None:
+        self.assert_proc(wait)
+        self._has_proc = False
+        self._streams.clear()
+
+    def do_connect(self, conn: Connect) -> None:
+        self.assert_proc(conn)
+        self.assert_no_stream(conn, conn.id)
+        assert conn.protocol_type == ProtocolType.TCP, \
+            f"{conn}: {conn.protocol_type} not supported"
+        self._streams.add(conn.id)
+
+    def do_send(self, send: Send) -> None:
+        self.assert_proc(send)
+        self.assert_stream(send, send.id)
+
+    def do_recv(self, recv: Recv) -> None:
+        self.assert_proc(recv)
+        self.assert_stream(recv, recv.id)
+
+    def do_cleanup(self, successful: bool) -> None:
+        if successful:
+            self.assert_no_proc(self._config_path)
+            assert not bool(self._streams)
+
+    def validate(self) -> None:
+        self.run()
